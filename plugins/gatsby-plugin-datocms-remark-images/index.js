e214504ef47448cdf3404a5789e8f9a986f67569
Maxime Doury <maxime.doury@gmail.com>
2018-11-04 23:29:27 +0100

    Refactoring of datocms remark plugin

--- plugins/gatsby-plugin-datocms-remark-images/index.js
+++ plugins/gatsby-plugin-datocms-remark-images/index.js
@@ -1,10 +1,13 @@
-const visitWithParents = require(`unist-util-visit-parents`)
+const visit = require(`unist-util-visit`)
+// TODO Remove lodash dependencies
 const { sortBy, minBy, defaults } = require(`lodash`)
 const Promise = require(`bluebird`)
 const sharp = require('sharp')
 const request = require('request-promise-native')
 
 module.exports = ({ markdownAST, pathPrefix, reporter }, pluginOptions) => {
+  // TODO Use roporter to report errors and achievements
+  // TODO Remove unnecessary plugin options
   const defaultProps = {
     maxWidth: 650,
     wrapperStyle: ``,
@@ -14,170 +17,195 @@ module.exports = ({ markdownAST, pathPrefix, reporter }, pluginOptions) => {
     pathPrefix,
     withWebp: false,
   }
-
+  // TODO Use plugin options
   const options = defaults(pluginOptions, defaultProps)
 
-  // This will only work for markdown syntax image tags
-  let markdownImageNodes = []
-  visitWithParents(markdownAST, `image`, node => {
+  // TODO Legacy: Replace with simple visit function (without parents)
+  const markdownImageNodes = []
+  visit(markdownAST, `image`, node => {
     markdownImageNodes.push({ node })
   })
+
+  const extractMetaData = async pipeline => {
+    const maxWidth = 2048
+    const { width, height, format } = await pipeline.metadata()
+    const aspectRatio = width / height
+    const displayWidth = Math.min(maxWidth, width)
+    const displayHeight = Math.round(displayWidth / aspectRatio)
+    return {
+      aspectRatio,
+      format,
+      height,
+      displayHeight,
+      displayWidth,
+      width,
+    }
+  }
+
+  const createDatoCmsFluidImages = (url, width, originalWidth, aspectRatio) => {
+    const sizes = [width * 3, width * 2, width * 1.5, width, width / 2, width / 3, width / 4]
+      .map(Math.round)
+      .filter(width => width < originalWidth)
+    sizes.push(originalWidth)
+    return sortBy(sizes).map(size => ({
+      width: size,
+      src: `${url}?auto=compress&fit=crop&fm=jpg&h=${Math.round(size / aspectRatio)}&w=${size}`,
+    }))
+  }
+
+  const createImageProps = (url, displayWidth, width, aspectRatio, format) => {
+    const images = createDatoCmsFluidImages(url, displayWidth, width, aspectRatio)
+    return {
+      sizes: `(max-width: ${displayWidth}px) 100vw, ${displayWidth}px`,
+      src: minBy(images, image => Math.abs(displayWidth - image.width)).src,
+      srcSet: images.map(image => `${image.src} ${image.width}w`).join(`,\n`),
+      srcSetType: `image/${format}`,
+    }
+  }
+
+  const createBase64Thumbnail = async (pipeline, width, aspectRatio) => {
+    const height = Math.max(1, width / aspectRatio)
+    const [buffer, info] = await pipeline.resize({ width, height }).toBufferAsync()
+    return `data:image/${info.format};base64,${buffer.toString('base64')}`
+  }
 
   const generateFluidImages = async ({ url, alt, title, reporter }) => {
     const imageBuffer = await request({ url, encoding: null })
     const pipeline = sharp(imageBuffer)
-
-    const { width, height, format } = await pipeline.metadata()
-    const aspectRatio = width / height
-
-    const maxWidth = 2048
-    const presentationWidth = Math.min(maxWidth, width)
-    const presentationHeight = Math.round(presentationWidth / aspectRatio)
-    const sizes = `(max-width: ${presentationWidth}px) 100vw, ${presentationWidth}px`
-
-    const fluidSizes = [presentationWidth]
-    fluidSizes.push(presentationWidth / 4)
-    fluidSizes.push(presentationWidth / 2)
-    fluidSizes.push(presentationWidth * 1.5)
-    fluidSizes.push(presentationWidth * 2)
-    fluidSizes.push(presentationWidth * 3)
-
-    const filteredSizes = fluidSizes.filter(size => size < width)
-    filteredSizes.push(width)
-    const sortedSizes = sortBy(filteredSizes)
-    const images = sortedSizes.map(size => {
-      const roundedSize = Math.round(size)
-      return {
-        width: roundedSize,
-        src: `${url}?auto=compress&fit=crop&fm=jpg&h=${Math.round(size / aspectRatio)}&w=${roundedSize}`,
-      }
-    })
-    const fallbackSrc = minBy(images, image => Math.abs(presentationWidth - image.width)).src
-
-    const srcSet = images.map(image => `${image.src} ${Math.round(image.width)}w`).join(`,\n`)
-    const srcSetType = `image/${format}`
-
-    const thumbnailWidth = 20
-    const thumbnailHeight = Math.max(1, 20 / aspectRatio)
-    const [buffer, info] = await pipeline.resize({ width: thumbnailWidth, height: thumbnailHeight }).toBufferAsync()
-    const base64 = `data:image/${info.format};base64,${buffer.toString('base64')}`
-
+    const { width, format, aspectRatio, displayWidth, displayHeight } = await extractMetaData(pipeline)
+    const base64 = await createBase64Thumbnail(pipeline, 20, aspectRatio)
+    const { sizes, src, srcSet, srcSetType } = createImageProps(url, displayWidth, width, aspectRatio, format)
     return {
       alt,
       aspectRatio,
       base64,
       originalSrc: url,
-      presentationWidth,
-      presentationHeight,
-      src: fallbackSrc,
+      displayWidth,
+      displayHeight,
+      sizes,
+      src,
       srcSet,
       srcSetType,
-      sizes,
       title,
     }
   }
+  const imgTag = ({ alt, className, dataSrc, src, style, title }) => `
+  <img 
+    ${alt ? `alt="${alt}"` : ``}
+    ${className ? `class="${className}"` : ``}
+    ${dataSrc ? `data-src="${dataSrc}"` : ``}
+    ${src ? `src="${src}"` : ``}
+    ${style ? `style="${style}"` : ``}
+    ${title ? `title="${title}"` : ``}
+    />`
 
-  const generateHtmlTag = fluid => {
-    // Calculate the paddingBottom %
-    const ratio = `${(1 / fluid.aspectRatio) * 100}%`
+  const pictureTag = ({ className, style, children }) => `
+  <picture 
+    ${className ? `class="${className}"` : ``}
+    ${style ? `style="${style}"` : ``}>
+    ${children.join('\n')}
+  </picture>`
 
-    // Generate default alt tag
-    if (!fluid.alt) {
-      const srcSplit = fluid.originalSrc.split(`/`)
-      const fileName = srcSplit[srcSplit.length - 1]
-      const fileNameNoExt = fileName.replace(/\.[^/.]+$/, ``)
-      fluid.alt = fileNameNoExt.replace(/[^A-Z0-9]/gi, ` `)
-    }
+  const sourceTag = ({ className, srcSet, dataSrcSet, sizes }) => `
+  <source 
+    ${className ? `class="${className}"` : ``}
+    ${srcSet ? `srcset="${srcSet}"` : ``}
+    ${dataSrcSet ? `data-srcset="${dataSrcSet}"` : ``}
+    ${sizes ? `sizes="${sizes}"` : ``} />`
 
+  // TODO Factorize (createPictureTag, createImageTag, createPictureTagStyle, createImageTagStyle, createDefaultAltTag)
+  const generateHtmlTag = ({
+    alt,
+    aspectRatio,
+    base64,
+    originalSrc,
+    displayWidth,
+    displayHeight,
+    sizes,
+    src,
+    srcSet,
+    srcSetType,
+    title,
+  }) => {
     const imageClass = `gatsby-resp-image-image`
     const baseImageStyle = `position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; object-fit: cover; object-position: center center;`
     const imageTagStyle = `${baseImageStyle} opacity: 1; transition: opacity 0.3s ease 0.2s;`
     const pictureTagStyle = `${baseImageStyle} opacity: 0; transition: opacity 0.5s ease 0s;`
+    const ratio = `${(1 / aspectRatio) * 100}%`
 
-    // Construct new image node w/ aspect ratio placeholder
-    let rawHTML = `
+    if (!alt) {
+      const srcSplit = originalSrc.split(`/`)
+      const fileName = srcSplit[srcSplit.length - 1]
+      const fileNameNoExt = fileName.replace(/\.[^/.]+$/, ``)
+      alt = fileNameNoExt.replace(/[^A-Z0-9]/gi, ` `)
+    }
+
+    const sourceEl = sourceTag({ dataSrcSet: srcSet, sizes: sizes })
+    const placeholderEl = imgTag({ alt, className: 'datocms-lazy-placeholder', src: base64, style: imageTagStyle })
+    const imageEl = imgTag({
+      alt,
+      className: imageClass,
+      dataSrc: src,
+      style: imageTagStyle,
+      title: title || alt,
+    })
+    const noScriptImageEl = imgTag({
+      alt,
+      className: imageClass,
+      src,
+      style: imageTagStyle,
+      title: title || alt,
+    })
+
+    return `
   <div class="datocms-remark-image-wrapper" style="position: relative; overflow: hidden;">
     <div style="padding-bottom: ${ratio};"></div>
-    <img class="datocms-lazy-placeholder"
-      alt="${fluid.alt}"
-      src="${fluid.base64}" 
-      style="${imageTagStyle}" />
-    <picture class="datocms-lazy-picture" 
-      style="${pictureTagStyle}">
-      <source
-        data-srcset="${fluid.srcSet}"
-        sizes="${fluid.sizes}">
-      </source>
-      <img
-        class="${imageClass}"
-        alt="${fluid.alt}"
-        title="${fluid.title || fluid.alt}"
-        data-src="${fluid.src}" />
-    </picture>
+    ${placeholderEl}
+    ${pictureTag({
+      className: 'datocms-lazy-picture',
+      style: pictureTagStyle,
+      children: [sourceEl, imageEl],
+    })}
     <noscript>
-      <picture>
-        <source
-          srcset="${fluid.srcSet}"
-          sizes="${fluid.sizes}">
-        </source>
-        <img
-          alt="${fluid.alt}"
-          class="${imageClass}"
-          src="${fluid.src}"
-          style="${imageTagStyle}"
-          title="${fluid.title || fluid.alt}" />
-      </picture>
+      ${pictureTag({ children: [sourceEl, noScriptImageEl] })}
     </noscript>
   </div>
   `
-    return rawHTML
   }
 
+  const matchDatoCmsUrl = url => /^https:\/\/www\.datocms-assets\.com\/\d+\/.*\.[a-z]{3}(\?.*)?$/gi.exec(url)
+  const sanitizeDatoCmsUrl = ([url, args]) => {
+    const idx = url.indexOf(args)
+    if (idx !== -1) {
+      return url.slice(0, idx)
+    }
+    return url
+  }
   const checkDatoCmsNode = node => {
     try {
-      let [
-        datoCmsAssetUrl,
-        datoCmsAssetArgumentString,
-      ] = /^https:\/\/www\.datocms-assets\.com\/\d+\/.*\.[a-z]{3}(\?.*)?$/gi.exec(node.url)
-      if (datoCmsAssetUrl) {
-        if (datoCmsAssetArgumentString && datoCmsAssetArgumentString.length) {
-          const argumentStringIndex = datoCmsAssetUrl.indexOf(datoCmsAssetArgumentString)
-          if (argumentStringIndex !== -1) {
-            datoCmsAssetUrl = datoCmsAssetUrl.slice(0, argumentStringIndex)
-          }
-        }
-        node.url = datoCmsAssetUrl
-        return node
-      } else {
-        return undefined
-      }
+      node.url = sanitizeDatoCmsUrl(matchDatoCmsUrl(node.url))
+      return node
     } catch (error) {
       console.error(error)
-      throw new Error(`Couldn't match image url: ${datoCmsAssetUrl}.\n${error}`)
+      throw new Error(`Couldn't match image url: ${node.url}.\n${error}`)
     }
   }
 
   return Promise.all(
-    // Simple because there is no nesting in markdown
     markdownImageNodes.map(
       ({ node }) =>
         new Promise(async (resolve, reject) => {
           const fileType = node.url.slice(-3)
-
-          // Ignore gifs as we can't process them,
-          // svgs as they are already responsive by definition
           const datoCmsNode = checkDatoCmsNode(node)
           if (datoCmsNode && fileType !== `gif` && fileType !== `svg`) {
             const rawHTML = await generateHtmlTag(await generateFluidImages(datoCmsNode))
-
             if (rawHTML) {
-              // Replace the image node with an inline HTML node.
               datoCmsNode.type = `html`
               datoCmsNode.value = rawHTML
             }
             return resolve(datoCmsNode)
           } else {
-            // Image isn't served from DatoCMS...
+            // Not an optimizable image served from DatoCMS...
             return resolve()
           }
         })
